11e4d07e5cf8b626f03450beb8db498011cf2217
Shahidul Shohid <shahidulislamshohid7@gmail.com>
2025-06-24 05:06:09 +0600

    aboutcompany page created is done

--- src/app/foremployer/aboutcompany/page.tsx
+++ src/app/foremployer/aboutcompany/page.tsx
@@ -10,6 +10,15 @@ const roleOptions = [
     { label: "Marketer", value: "marketer" },
     { label: "Other", value: "other" },
 ];
+const regionOptions = [
+    { label: "Bangladesh", value: "bangladesh" },
+    { label: "India", value: "india" },
+    { label: "United States", value: "usa" },
+    { label: "United Kingdom", value: "uk" },
+    { label: "Canada", value: "canada" },
+    { label: "Australia", value: "australia" },
+];
+
 
 function AboutCompany() {
     return (
@@ -24,19 +33,70 @@ function AboutCompany() {
                     </p>
                 </div>
                 <div className='space-y-6'>
+                    {/* company name */}
                     <FormInput label="Company Name" placeholder="SM Technology" />
-                    <div className="flex flex-col md:flex-row items-center justify-center gap-5">
+                    <div className="md:flex items-center justify-center gap-6">
+                        {/* industry type */}
                         <div className="w-full md:w-1/2">
                             <FormInput
                                 label="Industry type"
                                 placeholder="Tech, Marketing, Finance, etc."
                             />
                         </div>
+                        {/* what is your role */}
                         <div className="w-full md:w-1/2">
                             <DropdownInput label="What is your role in the company?"
                                 name="role"
-                                options={roleOptions}/>
+                                options={roleOptions} />
                         </div>
+                    </div>
+                    {/* company description */}
+                    <div>
+                        <label htmlFor="companyDescription" className="block text-sm font-medium text-gray-700 mb-1">
+                            Company Description
+                        </label>
+                        <textarea
+                            id="companyDescription"
+                            name="companyDescription"
+                            rows={5}
+                            placeholder="SM Technology"
+                            className="w-full p-3 border border-[#c2c2c2] rounded-md text-gray-700"
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-col md:flex-row items-center justify-center gap-6 gap-y-4">
+                    {/* Company Country/Region */}
+                    <div className="w-full md:w-[30%]">
+                        <DropdownInput
+                            label="Company Country/Region"
+                            name="role"
+                            options={regionOptions}
+                        />
+                    </div>
+
+                    {/* Company Address */}
+                    <div className="w-full md:w-[70%]">
+                        <FormInput
+                            label="Company Address"
+                            placeholder="Section-06, Mirpur, Dhaka"
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col md:flex-row gap-4">
+                    {/* city */}
+                    <div className="w-full md:w-1/3">
+                        <FormInput label="City" placeholder="Dhaka" />
+                    </div>
+
+                    {/* state */}
+                    <div className="w-full md:w-1/3">
+                        <FormInput label="State" placeholder="Dhaka" />
+                    </div>
+
+                    {/* zip code */}
+                    <div className="w-full md:w-1/3">
+                        <FormInput label="ZIP Code" placeholder="1216" />
                     </div>
                 </div>
             </div>
